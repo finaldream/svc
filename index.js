41db399ec72ef179c6ea40fd4d229d3546281d15
olivererdmann <o.erdmann@finaldream.de>
2015-09-30 14:53:40 +0200

    Relaces `libxmljs` with `xml2js`. Introduces verbosity-option.
    * Due to compilation issues of the libxml-depenceny on various system, I switched to `xml2js` as a plain JS-lib.
    * Introduced the `-v, --verbose`-option, wich provides different levels of verbosity, if increased.

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ var pJson  = require('./package.json');
 
 process.title = 'svc';
 
+function increaseVerbosity(v, total)
+{
+    return total + 1;
+}
+
 var desc = [
     pJson.description + ', Version ' + pJson.version,
     '',
@@ -15,12 +20,12 @@ var desc = [
     'The filenames will be used as CSS-selectors for the generated rules.'
 ].join('\n');
 
-
 cli.version(pJson.version)
     .description(desc)
     .usage('[options] <source-dir> <target-file>')
     .option('--prefix [value]', 'Prepended string to each selector in the resulting css.')
     .option('--write-dimensions', 'Write width and height as sass-variables to the resulting css.')
+    .option('-v, --verbose', 'Verbose output. -v = basic, -vv = detailed, -vvv = debug.', increaseVerbosity, 0)
     .parse(process.argv);
 
 if (!process.argv.slice(2).length) {
